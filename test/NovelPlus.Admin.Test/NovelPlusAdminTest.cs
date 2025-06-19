348d6dd0cba37f1876f94096d234f5b0864f2243
zhaoweijie <zhaoweijie1108@qq.com>
2025-06-19 18:31:17 +0800

    修改测试方法名称

---
+++ test/NovelPlus.Admin.Test/NovelPlusAdminTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace NovelPlus.Admin.Test
+{
+    public class NovelPlusAdminTest(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+    {
+        [Fact]
+        public void Test1()
+        {
+            var client = factory.CreateClient();
+        }
+    }
+}
