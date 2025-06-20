e1435354279ca79ee4359983a681fc987dc2c273
zhaoweijie <49708264+zhaoweijie1213@users.noreply.github.com>
2025-06-20 18:42:48 +0800

    docs: update portal repo section

---
+++ src/Portal/NovelPlus.Portal.Service.Domain/Repositories/IUserBookshelfRepository.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NovelPlus.Portal.Service.Domain.Entities;
+
+namespace NovelPlus.Portal.Service.Domain.Repositories;
+
+/// <summary>
+/// 用户书架仓储接口
+/// </summary>
+public interface IUserBookshelfRepository
+{
+    /// <summary>
+    /// 查询书架记录
+    /// </summary>
+    /// <param name="id">记录ID</param>
+    /// <returns>书架记录</returns>
+    Task<UserBookshelfEntity?> GetAsync(long id);
+
+    /// <summary>
+    /// 书架列表
+    /// </summary>
+    /// <param name="query">查询参数</param>
+    /// <returns>书架集合</returns>
+    Task<List<UserBookshelfEntity>> ListAsync(Dictionary<string, object> query);
+
+    /// <summary>
+    /// 书架数量
+    /// </summary>
+    /// <param name="query">查询参数</param>
+    /// <returns>数量</returns>
+    Task<int> CountAsync(Dictionary<string, object> query);
+
+    /// <summary>
+    /// 新增书架记录
+    /// </summary>
+    /// <param name="entity">书架记录</param>
+    Task<int> SaveAsync(UserBookshelfEntity entity);
+
+    /// <summary>
+    /// 更新书架记录
+    /// </summary>
+    /// <param name="entity">书架记录</param>
+    Task<int> UpdateAsync(UserBookshelfEntity entity);
+
+    /// <summary>
+    /// 删除书架记录
+    /// </summary>
+    /// <param name="id">记录ID</param>
+    Task<int> RemoveAsync(long id);
+
+    /// <summary>
+    /// 批量删除书架记录
+    /// </summary>
+    /// <param name="ids">记录ID集合</param>
+    Task<int> BatchRemoveAsync(long[] ids);
+
+    /// <summary>
+    /// 查询用户书架
+    /// </summary>
+    /// <param name="userId">用户ID</param>
+    /// <returns>书架集合</returns>
+    Task<List<UserBookshelfEntity>> ListBookShelfAsync(long userId);
+}
---
+++ src/Portal/NovelPlus.Portal.Service.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NovelPlus.Portal.Service.Domain.Entities;
+
+namespace NovelPlus.Portal.Service.Domain.Repositories;
+
+/// <summary>
+/// 用户仓储接口
+/// </summary>
+public interface IUserRepository
+{
+    /// <summary>
+    /// 查询用户
+    /// </summary>
+    /// <param name="id">用户ID</param>
+    /// <returns>用户信息</returns>
+    Task<UserEntity?> GetAsync(long id);
+
+    /// <summary>
+    /// 用户列表
+    /// </summary>
+    /// <param name="query">查询参数</param>
+    /// <returns>用户集合</returns>
+    Task<List<UserEntity>> ListAsync(Dictionary<string, object> query);
+
+    /// <summary>
+    /// 用户数量
+    /// </summary>
+    /// <param name="query">查询参数</param>
+    /// <returns>数量</returns>
+    Task<int> CountAsync(Dictionary<string, object> query);
+
+    /// <summary>
+    /// 新增用户
+    /// </summary>
+    /// <param name="user">用户实体</param>
+    Task<int> SaveAsync(UserEntity user);
+
+    /// <summary>
+    /// 更新用户
+    /// </summary>
+    /// <param name="user">用户实体</param>
+    Task<int> UpdateAsync(UserEntity user);
+
+    /// <summary>
+    /// 删除用户
+    /// </summary>
+    /// <param name="id">用户ID</param>
+    Task<int> RemoveAsync(long id);
+
+    /// <summary>
+    /// 批量删除用户
+    /// </summary>
+    /// <param name="ids">用户ID集合</param>
+    Task<int> BatchRemoveAsync(long[] ids);
+
+    /// <summary>
+    /// 增加用户余额
+    /// </summary>
+    /// <param name="userId">用户ID</param>
+    /// <param name="amount">金额</param>
+    Task AddUserBalanceAsync(long userId, int amount);
+}
