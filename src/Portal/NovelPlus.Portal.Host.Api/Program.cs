980ba421ef33ac7092be8364fe9abd7a3520b04b
zhaoweijie <zhaoweijie1108@qq.com>
2025-06-23 12:24:18 +0800

    添加基础组件

--- src/Portal/NovelPlus.Portal.Host.Api/Program.cs
+++ src/Portal/NovelPlus.Portal.Host.Api/Program.cs
@@ -1,6 +1,9 @@
+using EasyCaching.Core.Configurations;
+using EasyCaching.Redis;
 using NovelPlus.Shared.Config;
 using NSwag;
 using QYQ.Base.Common.IOCExtensions;
+using QYQ.Base.SnowId;
 using QYQ.Base.Swagger.Extension;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +21,38 @@ builder.AddQYQSwaggerAndApiVersioning(new OpenApiInfo()
 {
     Title = "NovelPlus"
 });
+//Ñ©»¨Id×é¼þ
+builder.Services.AddSnowIdRedisGenerator(builder.Configuration.GetSection("Redis").Get<RedisDBOptions>());
+//ÅäÖÃRedis
+builder.Services.AddEasyCaching(delegate (EasyCachingOptions options)
+{
+    options.UseRedis(delegate (RedisOptions config)
+    {
+        config.DBConfig = builder.Configuration.GetSection("Redis").Get<RedisDBOptions>();
+    }, "DefaultRedis").WithMessagePack();
+});
 
+#region  CAP
+
+builder.Services.AddCap(options =>
+{
+    var configuration = builder.Configuration;
+    options.UseRabbitMQ(config =>
+    {
+        config.HostName = configuration["CAP:RabbitMQ:HostName"]!;
+        config.Port = configuration.GetSection("CAP:RabbitMQ:Port").Get<int>();
+        config.UserName = configuration["CAP:RabbitMQ:UserName"]!;
+        config.Password = configuration["CAP:RabbitMQ:Password"]!;
+        config.ExchangeName = configuration["CAP:RabbitMQ:ExchangeName"]!;
+    });
+    options.UseMySql(opt =>
+    {
+        opt.ConnectionString = configuration["SysCap"]!;
+        opt.TableNamePrefix = AppDomain.CurrentDomain.FriendlyName;
+    });
+});
+
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
